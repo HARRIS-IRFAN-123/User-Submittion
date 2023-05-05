396426e7e15a3e1c1ac9fd0ad19a2eeef25f5fdc
Your Name <you@example.com>
2023-05-05 18:15:27 +0500

    added react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [password,setPassword]= useState("")
+  const [list,setList] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name,email,password);
+    const data = {name,email,password}
+    if(name && email && password){
+      setList((ls)=>[...ls,data]);
+      setName("");
+      setEmail("");
+      setPassword("");
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1>Welcome To User Registration Form</h1>
+        <form className="input" onSubmit={handleSubmit}>
+          <input name='name' placeholder='Name'value={name} onChange={(e)=> setName(e.target.value)}/>
+          <input name='email' placeholder='Email' value={email} onChange={(e)=>setEmail(e.target.value)}/>
+          <input name='password' placeholder='Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+          <button>Register</button> 
+        </form>
+            {
+              list?.map((a) =><div>
+                <li>{a.name}</li>
+                <li>{a.email}</li>
+                <li>{a.password}</li>
+                </div>)
+            }
+        </div>
   );
 }
 
